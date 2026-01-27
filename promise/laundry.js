27638169d8a8fb8e0d0abb32fc52e3e0922777ce
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-27 21:00:14 +0530

    promise using time

--- promise/laundry.js
+++ promise/laundry.js
@@ -1,52 +1,52 @@
-function loadClothes() {
+function loadClothes(time) {
   return new Promise((resolve) => {
     setTimeout(() => {
       console.log("Clothes are loaded into the washing machine");
       resolve();
-    }, 2000);
+    }, time);
   });
 }
-function startWashing() {
+function startWashing(time) {
   return new Promise((resolve) => {
     setTimeout(() => {
       console.log("Washing cycle started");
       resolve();
-    }, 3000);
+    }, time);
   });
 }
 
-function dryClothes() {
+function dryClothes(time) {
   return new Promise((resolve) => {
     setTimeout(() => {
       console.log("Clothes are dried");
       resolve();
-    }, 2000);
+    }, time);
   });
 }
 
-function foldClothes() {
+function foldClothes(time) {
   return new Promise((resolve) => {
     setTimeout(() => {
       console.log("Clothes are folded");
       resolve();
-    }, 1000);
+    }, time);
   });
 }
 
-function ironClothes() {
+function ironClothes(time) {
   return new Promise((resolve) => {
     setTimeout(() => {
       console.log("Clothes are ironed");
       resolve();
-    }, 2000);
+    }, time);
   });
 }
 
-loadClothes()
-  .then(() => startWashing())
-  .then(() => dryClothes())
-  .then(() => foldClothes())
-  .then(() => ironClothes())
+loadClothes(2000)
+  .then(() => startWashing(3000))
+  .then(() => dryClothes(2000))
+  .then(() => foldClothes(1000))
+  .then(() => ironClothes(2000))
   .then(() => {
     console.log("Laundry completed");
   })
