36bca0c7c1a181571d40452ab0c1c0aadc13b578
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-28 21:43:53 +0530

    add style

--- promise/react/reactIntro/intro.jsx
+++ promise/react/reactIntro/intro.jsx
@@ -1,3 +1,3 @@
-const element = React.createElement("h1", null, "React is awesome!");
+const element = React.createElement("h1", {style: {color: "blue"}}, "React is awesome!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
