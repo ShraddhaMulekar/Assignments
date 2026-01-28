86e361023fafdf2f152608261a599b2c9d1e6cc1
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-28 21:46:49 +0530

    add more tags and create with styling

--- promise/react/reactIntro/intro.jsx
+++ promise/react/reactIntro/intro.jsx
@@ -1,3 +1,6 @@
-const element = React.createElement("h1", {style: {color: "blue"}}, "React is awesome!");
+const element = React.createElement("div", {id:"container"}, [
+    React.createElement("h1", null, "Shraddha, I am from Nagpur, Maharashtra."),
+    React.createElement("h2", {style:{color:"navy", textAlign:"center"}}, "I love to code."),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
