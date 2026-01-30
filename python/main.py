fbfead78c77aed79074aa9356bb484d3b4dc38a2
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-30 12:36:52 +0530

    practice of variable, list, dist

--- python/main.py
+++ python/main.py
@@ -1,4 +1,17 @@
 name = "shraddha"
 age = 25
+married = True
+language = ["HTML", "CSS", "JS"]
+address = {
+    "city" : "Nagpur",
+    "state" : "Maharashtra",
+    "pincode" : 440034
+}
 
-print("name:", name, "age:", age)
+print(
+    f"My Name is {name}."
+    f"My Age is {age}."
+    f"I am from {address['city']}, {address['state']}- {address['pincode']}."
+    f"I learned {language[0]}, {language[1]}, {language[2]}."
+    f"I am {'Unmarried' if not married else 'Married'}."
+)
