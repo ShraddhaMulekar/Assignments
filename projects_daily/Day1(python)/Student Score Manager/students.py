6b1dc1f97a32a651b2b26df354256fa9ebff930d
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-30 14:46:48 +0530

    add project heading

--- projects_daily/Day1(python)/Student Score Manager/students.py
+++ projects_daily/Day1(python)/Student Score Manager/students.py
@@ -1,3 +1,5 @@
+#Student Score Manager
+
 students = []
 
 def add_student_data (name, mark):
@@ -10,9 +12,8 @@ def average_marks():
     total=0
     for student in students:
         total += student['mark']
-        print("total marks per student:", student['mark'])
-
-    print("total marks all students:", total)
+        # print("total marks per student:", student['mark'])
+    # print("total marks all students:", total)
     
     return total / len(students)
 
@@ -21,7 +22,7 @@ try:
     add_student_data("Neha", 90)
     add_student_data("Shraddha", int("85"))
 
-    # avg = average_marks()
-    print("Average Marks:", average_marks())
+    avg = average_marks()
+    print("Average Marks:", avg)
 except:
     print("Invalid data entered")
