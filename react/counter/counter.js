6ed69666f7caea634d431626d61de687b429f162
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-30 21:22:41 +0530

    add use state

--- react/counter/counter.js
+++ react/counter/counter.js
@@ -1,18 +1,23 @@
+import { useState } from "react"
+
 let count = 0
 function Counter(){
-    const increment = ()=>{
-        count ++
-        App()
-    }
-    const decrement = ()=>{
-        count --
-        App()
-    }
+    const[count, setCount] = useState(0)
+    // const increment = ()=>{
+    //     count ++
+    //     App()
+    // }
+    // const decrement = ()=>{
+    //     count --
+    //     App()
+    // }
     return(
         <>
         <h1>Counter : {count}</h1>
         <button onClick={increment}>Inc</button>
-        <button onClick={decrement}>Dec</button>
+        <button onClick={decrement}>Dec</button>        
+        <button onClick={setCount(count++)}>Inc</button>
+        <button onClick={setCount(count--)}>Dec</button>
         </>
 
     )
