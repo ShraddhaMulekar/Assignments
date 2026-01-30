134c4adbba2a85b54c5e111f40180ccb393590ec
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-30 14:03:45 +0530

    mini projects using def, dist, try-except block in python

---
+++ projects_daily/Day1(python)/Student Score Manager/students.py
@@ -0,0 +1,27 @@
+students = []
+
+def add_student_data (name, mark):
+    students.append({
+        "name":name,
+        "mark": mark
+    })
+
+def average_marks():
+    total=0
+    for student in students:
+        total += student['mark']
+        print("total marks per student:", student['mark'])
+
+    print("total marks all students:", total)
+    
+    return total / len(students)
+
+try:
+    add_student_data("Amit", 80)
+    add_student_data("Neha", 90)
+    add_student_data("Shraddha", int("85"))
+
+    # avg = average_marks()
+    print("Average Marks:", average_marks())
+except:
+    print("Invalid data entered")
