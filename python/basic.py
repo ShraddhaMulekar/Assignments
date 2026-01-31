af24104856b7b2dba4847026acbfab97e224dc4d
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-31 19:39:58 +0530

    add openai basic code

--- python/basic.py
+++ python/basic.py
@@ -1,9 +1,9 @@
-# from openai import OpenAI
-# client = OpenAI()
+from openai import OpenAI
+client = OpenAI()
 
-# response = client.responses.create(
-#     model="gpt-5-nano",
-#     input="Write a one-sentence bedtime story about a unicorn."
-# )
+response = client.responses.create(
+    model="gpt-5-nano",
+    input="Write a one-sentence bedtime story about a unicorn."
+)
 
-# print(response.output_text)
+print(response.output_text)
