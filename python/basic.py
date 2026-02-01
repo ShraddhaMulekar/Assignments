104ba9d573f0c24b38c623c55340ef7437008458
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-01 14:05:19 +0530

    install croq

--- python/basic.py
+++ python/basic.py
@@ -1,9 +1,19 @@
-from openai import OpenAI
-client = OpenAI()
 
-response = client.responses.create(
-    model="gpt-5-nano",
-    input="Write a one-sentence bedtime story about a unicorn."
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1",
 )
 
+# response = client.chat.completions.create(
+response = client.responses.create(
+    # messages=[{"role": "user", "content": "Explain the importance of fast language models"}],
+    input="Explain the importance of fast language models",
+    model="llama-3.3-70b-versatile",
+)
+# print(response.choices[0].message.content)
 print(response.output_text)
