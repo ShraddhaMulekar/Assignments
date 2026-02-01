ce549d5b254e2459c824ffdfb2848167624b080e
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-01 18:21:47 +0530

    Utility to Load Prompts

--- projects_daily/Day3(AI-project)/ai-study-buddy/main.py
+++ projects_daily/Day3(AI-project)/ai-study-buddy/main.py
@@ -1,4 +1,12 @@
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
+from config import MODEL_NAME, TEMPERATURES
 
 load_dotenv()
+
+def load_prompt(file_name, topic):
+    with open(f"prompts/{file_name}", "r") as f:
+        template=f.read()
+    return template.replace("{topic}", topic)
+
