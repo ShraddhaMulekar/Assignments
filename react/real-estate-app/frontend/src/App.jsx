48f31963dc672490818ad6e73788aa03a24bc0a4
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-23 21:12:37 +0530

    make component for frontend side

--- react/real-estate-app/frontend/src/App.jsx
+++ react/real-estate-app/frontend/src/App.jsx
@@ -1,12 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Services from './components/services'
 // import './App.css'
 
 function App() {
 
   return (
     <>
-      react
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />  
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/services' element={<Services />} />
+      </Routes>
     </>
   )
 }
