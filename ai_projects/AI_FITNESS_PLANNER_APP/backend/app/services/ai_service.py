b1aebcecbe0964eab5bdc6d5c8b32ee89d0a608d
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-04 20:25:30 +0530

    model error fix

--- ai_projects/AI_FITNESS_PLANNER_APP/backend/app/services/ai_service.py
+++ ai_projects/AI_FITNESS_PLANNER_APP/backend/app/services/ai_service.py
@@ -8,7 +8,7 @@ def generate_ai_response(prompt : str):
         messages = [
             {"role":"user", "content":prompt}
         ],
-        model = "llama3-8b-8192"
+        model = "openai/gpt-oss-20b"
     )
 
     return chat_completion.choices[0].message.content
