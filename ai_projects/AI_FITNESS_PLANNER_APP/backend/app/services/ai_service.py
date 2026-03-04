bff7dc5c14889f27dc6f84a5ae2926c5eee6d97e
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-04 20:08:03 +0530

    This is AI integration layer.

---
+++ ai_projects/AI_FITNESS_PLANNER_APP/backend/app/services/ai_service.py
@@ -0,0 +1,14 @@
+from groq import Groq
+from app.core.config import GROQ_API_KEY
+
+client = Groq(api_key=GROQ_API_KEY)
+
+def generate_ai_response(prompt : str):
+    chat_completion = client.chat.completions.create(
+        messages = [
+            {"role":"user", "content":prompt}
+        ],
+        model = "llama3-8b-8192"
+    )
+
+    return chat_completion.choices[0].message.content
