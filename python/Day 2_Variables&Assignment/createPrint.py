54082f2d014a97371a323d7aab9819086e90b084
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-23 12:53:38 +0530

    2.1 Create and print variables

---
+++ python/Day 2_Variables&Assignment/createPrint.py
@@ -0,0 +1,12 @@
+# Create variables for your name, age, and favorite color, then print them.
+
+name="Shraddha"
+BOY=1998
+favorite_color="White"
+
+print("name:", name)
+print("BOY:", BOY)
+print("favorite_color:", favorite_color)
+
+years_to_30 = 2026 - BOY
+print(f"years_to_30: {years_to_30}")
