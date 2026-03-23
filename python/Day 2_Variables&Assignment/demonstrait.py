a8852ea815de08353889e01beee28a8cea04d38e
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-23 16:06:15 +0530

    Demonstrate Variable Reassignment

---
+++ python/Day 2_Variables&Assignment/demonstrait.py
@@ -0,0 +1,19 @@
+# Show how variables can change their values and even types.
+
+counter = 0
+print(f"Initial counter: {counter}, type: {type(counter)}")
+
+counter += 5
+print(f"+5 counter: {counter}, type: {type(counter)}")
+
+data = 100
+print(f"\ndata as integer: {data}, type: {type(data)}")
+
+data = "hello"
+print(f"\ndata as integer: {data}, type: {type(data)}")
+
+data = 3.14
+print(f"\ndata as integer: {data}, type: {type(data)}")
+
+data = [100, 2, 6]
+print(f"\ndata as integer: {data}, type: {type(data)}")
