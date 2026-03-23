3afbd2e693c5425fea305d69716ce64abac72ed7
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-23 12:35:26 +0530

    practice for Print Function Basics

---
+++ python/day1_print_comments_syntax/index.py
@@ -0,0 +1,11 @@
+print("Welcome to Python!")
+
+# Print Function Basics
+print("Hello world!")
+
+# Practice Problem 1.1: Print your name
+# Task: Use the print function to display your name
+# Hint: Remember to use quotes around text
+
+# Your code here:
+print("Shraddha")
