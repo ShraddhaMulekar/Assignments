7a9b304b12a7181f67b12eb9f20620440fea9f05
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-30 14:46:49 +0530

    string creation

---
+++ python/day4_String_Data_Type/stringCreation.py
@@ -0,0 +1,11 @@
+single_quotes = 'Hello World'
+double_quotes = "Hello World"
+triple_single = '''This is a
+multi-line string'''
+triple_double = """This is also a
+                    multi-line string"""
+
+print(single_quotes)
+print(double_quotes)
+print(triple_single)
+print(triple_double)
